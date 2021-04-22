80f00da5dbb254b6152b244622df3f4723ba8eec
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-04-23 01:54:29 +0300

    refactor

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public IResult Add(Category category)
+        {
+            _categoryDal.Add(category);
+
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Category>> GetAll()
+        {
+            var result = _categoryDal.GetAll();
+
+            return new SuccessDataResult<List<Category>>(result);
+        }
+
+        public IDataResult<Category> GetById(int id)
+        {
+            var result = _categoryDal.Get(c => c.Id == id);
+
+            return new SuccessDataResult<Category>(result);
+        }
+
+        public IResult Update(Category category)
+        {
+            _categoryDal.Update(category);
+
+            return new SuccessResult();
+        }
+    }
+}
--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -7,6 +7,9 @@ namespace Entities.Concrete
 {
     public class Category : IEntity
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool Active { get; set; }
     }
 }
