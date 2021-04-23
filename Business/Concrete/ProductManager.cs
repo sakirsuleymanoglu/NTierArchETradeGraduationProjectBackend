ece8c8ad0764963c2400353eae05030e3e4841d4
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-04-23 21:57:56 +0300

    23 nisan refactor

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,10 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Business.Concrete
 {
@@ -12,57 +12,138 @@ namespace Business.Concrete
     {
         private readonly IProductDal _productDal;
 
-        public ProductManager(IProductDal productDal)
+        private readonly ICategoryService _categoryService;
+
+        private readonly IBrandService _brandService;
+
+        public ProductManager(IProductDal productDal, ICategoryService categoryService, IBrandService brandService)
         {
             _productDal = productDal;
+
+            _categoryService = categoryService;
+
+            _brandService = brandService;
         }
 
         public IResult Add(Product product)
         {
-            //iş kuralları
+            _productDal.Add(product);
 
-            if (product.CategoryId == 1)
-            {
-                return new ErrorResult("Bu kategoriye ürün kabul edilmiyor");
-            }
-            else
-            {
-                _productDal.Add(product);
-                return new SuccessResult("Ürün eklendi");
-            }
-
+            return new SuccessResult();
         }
 
         public IResult Delete(Product product)
         {
-            throw new NotImplementedException();
+            var result = BusinessRules.Run(CheckIfExistsProduct(product.Id));
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            _productDal.Delete(product);
+
+            return new SuccessResult();
         }
 
         public IDataResult<List<Product>> GetAll()
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),"Ürünler listelendi");
+            var result = _productDal.GetAll();
+
+            return new SuccessDataResult<List<Product>>(result);
         }
 
         public IDataResult<List<Product>> GetAllByBrand(int brandId)
         {
-            throw new NotImplementedException();
+            var result = BusinessRules.Run(CheckIfExistsBrand(brandId));
+
+            if (result != null)
+            {
+                return new ErrorDataResult<List<Product>>(result.Message);
+            }
+
+            var productListByBrand = _productDal.GetAll(p => p.BrandId == brandId);
+
+            return new SuccessDataResult<List<Product>>(productListByBrand);
         }
 
         public IDataResult<List<Product>> GetAllByCategory(int categoryId)
         {
+            var result = BusinessRules.Run(CheckIfExistsCategory(categoryId));
+
+            if (result != null)
+            {
+                return new ErrorDataResult<List<Product>>(result.Message);
+            }
+
+            var productListByCategory = _productDal.GetAll(p => p.CategoryId == categoryId);
+
             return new SuccessDataResult<List<Product>>
-                (_productDal.GetAll(p=>p.CategoryId==categoryId), 
-                "Ürünler listelendi");
+                (productListByCategory);
         }
 
         public IDataResult<Product> GetById(int id)
         {
-            throw new NotImplementedException();
+            var result = BusinessRules.Run(CheckIfExistsProduct(id));
+
+            if (result != null)
+            {
+                return new ErrorDataResult<Product>(result.Message);
+            }
+
+            var product = _productDal.Get(p => p.Id == id);
+
+            return new SuccessDataResult<Product>(product);
         }
 
         public IResult Update(Product product)
         {
-            throw new NotImplementedException();
+            var result = BusinessRules.Run(CheckIfExistsProduct(product.Id));
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            _productDal.Update(product);
+
+            return new SuccessResult();
+        }
+
+        public IResult CheckIfExistsProduct(int productId)
+        {
+            var result = _productDal.Get(p => p.Id == productId);
+
+            if (result == null)
+            {
+                return new ErrorResult();
+            }
+
+            return new SuccessResult();
+        }
+
+        public IResult CheckIfExistsBrand(int brandId)
+        {
+            var result = _brandService.GetById(brandId);
+
+            if (result == null)
+            {
+                return new ErrorResult();
+            }
+
+            return new SuccessResult();
+        }
+
+        public IResult CheckIfExistsCategory(int categoryId)
+        {
+            var result = _categoryService.GetById(categoryId);
+
+            if (result == null)
+            {
+                return new ErrorResult();
+            }
+
+            return new SuccessResult();
         }
     }
 }
