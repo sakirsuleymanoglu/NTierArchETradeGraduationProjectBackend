8346759db211f8c18dd65ef764bb34fbbc9c8df5
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-04-23 07:10:21 +0300

    23 nisan

--- Entities/Concrete/OrderDetail.cs
+++ Entities/Concrete/OrderDetail.cs
@@ -9,6 +9,6 @@ namespace Entities.Concrete
         public int OrderId { get; set; }
         public int ProductId { get; set; }
         public int Count { get; set; }
-        public decimal SalePrice { get; set; }
+        public int SalePrice { get; set; }
     }
 }
