e56122114ec9b074a2daed41ad6550a6c0c63e1d
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-04-23 03:29:27 +0300

    23 nisan

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -9,7 +9,11 @@ namespace Business.Abstract
     public interface IProductService
     {
         IDataResult<List<Product>> GetAll();
+        IDataResult<Product> GetById(int id);
         IDataResult<List<Product>> GetAllByCategory(int categoryId);
+        IDataResult<List<Product>> GetAllByBrand(int brandId);
         IResult Add(Product product);
+        IResult Delete(Product product);
+        IResult Update(Product product);
     }
 }
