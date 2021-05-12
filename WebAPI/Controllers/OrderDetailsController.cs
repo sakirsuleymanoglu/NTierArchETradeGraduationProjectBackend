5d7056ce3128c1c04a6a8a5963bfb91d2af17039
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-05-12 19:00:52 +0300

    12 mayıs

---
+++ WebAPI/Controllers/OrderDetailsController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        private IOrderDetailService _orderDetailService;
+
+        public OrderDetailsController(IOrderDetailService orderDetailService)
+        {
+            _orderDetailService = orderDetailService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _orderDetailService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _orderDetailService.GetById(id);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyorder")]
+        public IActionResult GetByOrder(int orderId)
+        {
+            var result = _orderDetailService.GetByOrder(orderId);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(OrderDetail orderDetail)
+        {
+            var result = _orderDetailService.Add(orderDetail);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(OrderDetail orderDetail)
+        {
+            var result = _orderDetailService.Delete(orderDetail);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(OrderDetail orderDetail)
+        {
+            var result = _orderDetailService.Update(orderDetail);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
